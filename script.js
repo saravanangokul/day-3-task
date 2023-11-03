539eedd4d7d27295c692c516e79485b0f7773bf6
saravanangokul <kksgokul43@gmail.com>
2023-11-03 13:05:25 +0530

    code change

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+// //qus1:-
+// //how to compare two JSON have the same properties without order?
+
+let ob1={name:"person 1",age:5};
+let obj2={age:5,name:"person 1"};
+let JSON1=JSON.stringify(ob1);
+let JSON2=JSON.stringify(obj2);
+if(JSON1===JSON2){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
 // qus:-2
 // use the rest countries' APL URL->https://restcountries.com/v3.1/all and the display all the country flags in the console 
-
 
 var b = new XMLHttpRequest();
 b.open("GET","https://restcountries.com/v3.1/all",true)
@@ -11,7 +23,7 @@ b.onload = function(){
     var result = JSON.parse(data);
     //console.log(result);
     for(var i=0;i<result.length;i++){
-        console.log(result[i].name.common,result[i].flag);
+        console.log(result[i].flag);
     }
 }
 
@@ -26,9 +38,9 @@ request.send();
 request.onload = function(){
     var a = request.response;
     //console.log(data);
-    var result = JSON.parse(a);
+    var result1= JSON.parse(a);
     //console.log(result);
-    for(var i=0;i<result.length;i++){
-        console.log(result[i].region,result[i].subregion,result[i].population);
-    }
+    for(var i=0;i<result1.length;i++){
+        console.log(result1[i].name.common,result1[i].region,result1[i].subregion,result1[i].population);
+    } 
 }
